d0ad14f3281e209157d62dce73a15efcf1f2a954
joe@codeherent.co.uk <joe@codeherent.co.uk>
2019-07-08 12:05:51 +0000

    test

--- env.tf
+++ env.tf
@@ -1,10 +1,13 @@
 variable "aws_access_key" {}
+
 variable "aws_secret_key" {}
-variable "key_name" {}
+
+variable "key_name_test" {}
 
 variable "region" {
   default = "us-east-1"
 }
+
 variable "instance_type" {
   default = "t2.micro"
 }
@@ -18,52 +21,58 @@ provider "aws" {
 resource "random_pet" "name" {}
 
 resource "aws_security_group" "Restricted" {
-  name = "${random_pet.name.id}"
+  name        = "${random_pet.name.id}"
   description = "Allow SSH, HTTP and HTTPS."
-
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = 6
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = 6
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 80
+    to_port   = 80
+    protocol  = 6
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
   }
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 443
+    to_port   = 443
+    protocol  = 6
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
   }
-
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
   }
-
   tags {
     Name = "pacman"
   }
 }
 
 resource "aws_instance" "web" {
-  ami = "ami-5679a229"
+  ami           = "ami-5679a229"
   instance_type = "${var.instance_type}"
   vpc_security_group_ids = [
-    "${aws_security_group.Restricted.id}"
+    "${aws_security_group.Restricted.id}",
   ]
   key_name = "${var.key_name}"
   tags {
     Name = "${random_pet.name.id}"
   }
-  depends_on = ["aws_security_group.Restricted"]
+  depends_on = [
+    "aws_security_group.Restricted",
+  ]
 }
 
 resource "aws_route53_record" "www" {
@@ -71,8 +80,12 @@ resource "aws_route53_record" "www" {
   name    = "pacman.codeherent.co.uk"
   type    = "A"
   ttl     = "300"
-  records = ["${aws_instance.web.public_ip}"]
-  depends_on = ["aws_instance.web"]
+  records = [
+    "${aws_instance.web.public_ip}",
+  ]
+  depends_on = [
+    "aws_instance.web",
+  ]
 }
 
 output "ip" {
